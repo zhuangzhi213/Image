5fcb5945a00bed4585f21d30a228eb58d9d6cedb
zhuangzhi <1716571003@qq.com>
2024-09-13 15:40:22 +0800

    测试

--- zz.js
+++ zz.js
@@ -4,9 +4,10 @@ const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const filename = path.join(__dirname, 'img.txt');
+const imgDir = path.join(__dirname, 'images', 'img', 'card'); // 图片所在的目录
+const filename = path.join(__dirname, 'images', 'img', 'img.txt');
 
-app.get('/api/img', (req, res) => {
+app.get('/api/file', (req, res) => {
     if (!fs.existsSync(filename)) {
         return res.status(404).send('文件不存在');
     }
@@ -47,6 +48,37 @@ app.get('/api/img', (req, res) => {
     fsStream.on('error', (error) => {
         console.error('Error reading file:', error);
         res.status(500).send('无法读取文件');
+    });
+});
+
+app.get('/api/dir', (req, res) => {
+    fs.readdir(imgDir, (err, files) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send('无法读取目录');
+        }
+
+        // 过滤只包含图片文件
+        const imageFiles = files.filter(file => /\.(jpg|jpeg|png|gif)$/i.test(file));
+
+        if (imageFiles.length === 0) {
+            return res.status(404).send('没有找到图片');
+        }
+
+        // 从数组中随机选择一张图片
+        const pic = imageFiles[Math.floor(Math.random() * imageFiles.length)];
+
+        // 返回指定格式
+        const type = req.query.type;
+
+        switch (type) {
+            case 'json':
+                res.setHeader('Content-Type', 'application/json');
+                res.send(JSON.stringify({ pic: `/images/img/card/${pic}` }));
+                break;
+            default:
+                res.redirect(302, `/images/img/card/${pic}`);
+        }
     });
 });
 
