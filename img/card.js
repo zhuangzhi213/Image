2b24a1702a2d535cba2e7a35dd6f86aad5bfdde1
zhuangzhi <1716571003@qq.com>
2024-09-13 15:22:21 +0800

    测试

--- img/card.js
+++ img/card.js
@@ -4,32 +4,24 @@ const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const filename = path.join(__dirname, 'img.txt');
+const imgDir = path.join(__dirname, 'img/card'); // 图片所在的目录
 
-app.get('/api/random-image-from-txt', (req, res) => {
-    if (!fs.existsSync(filename)) {
-        return res.status(404).send('文件不存在');
-    }
-
-    const pics = [];
-    const fsStream = fs.createReadStream(filename, { encoding: 'utf8' });
-
-    fsStream.on('data', (chunk) => {
-        chunk.split('\n').forEach((line) => {
-            line = line.trim();
-            if (line !== '') {
-                pics.push(line);
-            }
-        });
-    });
-
-    fsStream.on('end', () => {
-        if (pics.length === 0) {
-            return res.status(404).send('没有找到图片链接');
+app.get('/api/card', (req, res) => {
+    fs.readdir(imgDir, (err, files) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send('无法读取目录');
         }
 
-        // 从数组中随机选择一个链接
-        const pic = pics[Math.floor(Math.random() * pics.length)];
+        // 过滤只包含图片文件
+        const imageFiles = files.filter(file => /\.(jpg|jpeg|png|gif)$/i.test(file));
+
+        if (imageFiles.length === 0) {
+            return res.status(404).send('没有找到图片');
+        }
+
+        // 从数组中随机选择一张图片
+        const pic = imageFiles[Math.floor(Math.random() * imageFiles.length)];
 
         // 返回指定格式
         const type = req.query.type;
@@ -37,16 +29,11 @@ app.get('/api/random-image-from-txt', (req, res) => {
         switch (type) {
             case 'json':
                 res.setHeader('Content-Type', 'application/json');
-                res.send(JSON.stringify({ pic }));
+                res.send(JSON.stringify({ pic: `/img/card/${pic}` }));
                 break;
             default:
-                res.redirect(302, pic);
+                res.redirect(302, `/img/card/${pic}`);
         }
-    });
-
-    fsStream.on('error', (error) => {
-        console.error('Error reading file:', error);
-        res.status(500).send('无法读取文件');
     });
 });
 
--- img/index.js
+++ img/index.js
@@ -4,24 +4,32 @@ const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const imgDir = path.join(__dirname, 'img'); // 图片所在的目录
+const filename = path.join(__dirname, 'img.txt');
 
-app.get('/api/random-image', (req, res) => {
-    fs.readdir(imgDir, (err, files) => {
-        if (err) {
-            console.error(err);
-            return res.status(500).send('无法读取目录');
+app.get('/api/img', (req, res) => {
+    if (!fs.existsSync(filename)) {
+        return res.status(404).send('文件不存在');
+    }
+
+    const pics = [];
+    const fsStream = fs.createReadStream(filename, { encoding: 'utf8' });
+
+    fsStream.on('data', (chunk) => {
+        chunk.split('\n').forEach((line) => {
+            line = line.trim();
+            if (line !== '') {
+                pics.push(line);
+            }
+        });
+    });
+
+    fsStream.on('end', () => {
+        if (pics.length === 0) {
+            return res.status(404).send('没有找到图片链接');
         }
 
-        // 过滤只包含图片文件
-        const imageFiles = files.filter(file => /\.(jpg|jpeg|png|gif)$/i.test(file));
-
-        if (imageFiles.length === 0) {
-            return res.status(404).send('没有找到图片');
-        }
-
-        // 从数组中随机选择一张图片
-        const pic = imageFiles[Math.floor(Math.random() * imageFiles.length)];
+        // 从数组中随机选择一个链接
+        const pic = pics[Math.floor(Math.random() * pics.length)];
 
         // 返回指定格式
         const type = req.query.type;
@@ -29,11 +37,16 @@ app.get('/api/random-image', (req, res) => {
         switch (type) {
             case 'json':
                 res.setHeader('Content-Type', 'application/json');
-                res.send(JSON.stringify({ pic: `/img/${pic}` }));
+                res.send(JSON.stringify({ pic }));
                 break;
             default:
-                res.redirect(302, `/img/${pic}`);
+                res.redirect(302, pic);
         }
+    });
+
+    fsStream.on('error', (error) => {
+        console.error('Error reading file:', error);
+        res.status(500).send('无法读取文件');
     });
 });
 
